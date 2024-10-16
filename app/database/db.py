b4d2a23a17e6292a6837b718366bd081f3940941
Agustin Aused <agus.aused@gmail.com>
2024-10-16 20:41:06 -0300

    Refactor database connection and session handling

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,24 @@
-﻿from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+﻿from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+
+# Crear el motor asincrónico para la conexión
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# Crear una sesión asincrónica
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+# Base para las clases de modelos
 Base = declarative_base()
+
+# Función para obtener la sesión de la base de datos
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
