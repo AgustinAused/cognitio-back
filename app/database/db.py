d279c08cdb62f57d5a5198e3f75d1ba7dc8d4e44
Agustin Aused <agus.aused@gmail.com>
2024-10-16 20:38:44 -0300

    Update .gitignore and db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,8 @@
-﻿# ConexiÃ³n a la base de datos con SQLAlchemy
-from sqlalchemy import create_engine
+﻿from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambia esto a la URL de tu base de datos
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
