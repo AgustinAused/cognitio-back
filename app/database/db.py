0bf5a63dae71d639b9b12ee049589a0c6d94ff4d
Agustin Aused <agus.aused@gmail.com>
2024-10-17 11:22:42 -0300

    Refactor db to add function for creating database tables

--- app/database/db.py
+++ app/database/db.py
@@ -22,3 +22,7 @@ async def get_db():
     async with SessionLocal() as session:
         yield session
 
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
