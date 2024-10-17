f4fbca9efd20129d4eb1e4e26a8dc060993ea486
Agustin Aused <agus.aused@gmail.com>
2024-10-17 10:02:56 -0300

    Add User model to Models

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,13 @@
-﻿class ExampleModel:
-    def __init__(self, id: int, name: str):
-        self.id = id
-        self.name = name
+﻿from sqlalchemy import Column, Integer, String, Boolean
+from app.database.db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+
