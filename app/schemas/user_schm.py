4d0bc7db98c1598e31bf7a1ab4980519ff9a7ab2
Agustin Aused <agus.aused@gmail.com>
2024-10-17 10:08:02 -0300

    Add UserCreate, UserResponse, and UserLogin models to user schema

---
+++ app/schemas/user_schm.py
@@ -0,0 +1,19 @@
+﻿from pydantic import BaseModel
+
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
