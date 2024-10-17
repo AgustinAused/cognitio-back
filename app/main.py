6f36fad697142ea8a3ddd91e8ff9f25708970e9b
Agustin Aused <agus.aused@gmail.com>
2024-10-17 11:16:32 -0300

    Refactor main to include user router and create database tables on startup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 ﻿from fastapi import FastAPI
-from app.routers import example_router
+from app.routers import user_r
+from app.database.db import create_db_and_tables
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def on_startup():
+     await create_db_and_tables()
+
 # Incluir routers (rutas)
-app.include_router(example_router.router)
+app.include_router(user_r.router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to FastAPI Project Template!"}
+    return {"message": "Welcome to FastAPI Project!"}
