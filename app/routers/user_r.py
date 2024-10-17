c8d3ae354563dd06b2491aefe33e3cd362086576
Agustin Aused <agus.aused@gmail.com>
2024-10-17 10:29:51 -0300

    Add user router for user registration, login, and token refreshing

---
+++ app/routers/user_r.py
@@ -0,0 +1,20 @@
+from app.controllers import user_c
+from fastapi import Depends, APIRouter
+from app.database.db import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.user_schm import UserCreate, UserLogin, UserResponse
+
+router = APIRouter()
+
+@router.post("/register", response_model= UserResponse)
+async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    return await user_c.register_user(user, db)
+
+
+@router.post("/login")
+async def login_user(user: UserLogin, db: AsyncSession = Depends(get_db)):
+    return await user_c.login_user(user, db)
+
+@router.post("/refresh")
+async def refresh_access_token(refresh_token: str):
+    return await user_c.refresh_access_token(refresh_token)
