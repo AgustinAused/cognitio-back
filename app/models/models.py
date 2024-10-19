09d84cf6f07d223985c6cb187179b77f8afd4fad
Agustin Aused <agus.aused@gmail.com>
2024-10-19 10:05:57 -0300

    Refactor models to include ProgressLevel and establish relationship with User

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,7 @@
-﻿from sqlalchemy import Column, Integer, String, Boolean
+﻿from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from app.database.db import Base
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -9,5 +11,28 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+
+     # Relación con los niveles de progreso
+    progress_levels = relationship("ProgressLevel", back_populates="user")
+
+
+class ProgressLevel(Base):
+    __tablename__ = 'progress_levels'
+
+    id = Column(Integer, primary_key=True, index=True)
+    level = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  
+    completed_at = Column(DateTime, nullable=True)  
+
+    ex1_completed = Column(Boolean, nullable=False, default=False)
+    ex2_completed = Column(Boolean, nullable=False, default=False)
+    ex3_completed = Column(Boolean, nullable=False, default=False)
+    ex4_completed = Column(Boolean, nullable=False, default=False)
+    ex5_completed = Column(Boolean, nullable=False, default=False)
+
+    # Relación con el usuario
+    user = relationship("User", back_populates="progress_levels")
+
+
 
 
