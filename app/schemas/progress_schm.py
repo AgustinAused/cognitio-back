6ea1dcead1344b1798f0387e9de7bddfc55ae014
Agustin Aused <agus.aused@gmail.com>
2024-10-23 15:02:59 -0300

    Refactor ProgressSchema to include individual completion status for each exercise

--- app/schemas/progress_schm.py
+++ app/schemas/progress_schm.py
@@ -4,18 +4,25 @@ from typing import List, Optional
 
 
 class ProgressBase(BaseModel):
-    user_id: int
     level: int
 
 
 class ProgressCreated(ProgressBase):
-    exercises: List[bool]
     completed_at: Optional[str] = None
+    ex1_completed = bool
+    ex2_completed = bool
+    ex3_completed = bool
+    ex4_completed = bool
+    ex5_completed = bool
 
 
 class ProgressUpdate(ProgressBase):
-    exercises: Optional[List[bool]]
     completed_at: Optional[str] = None
+    ex1_completed = Optional[bool]
+    ex2_completed = Optional[bool]
+    ex3_completed = Optional[bool]
+    ex4_completed = Optional[bool]
+    ex5_completed = Optional[bool]
 
 
 class ProgressOut(ProgressBase):
