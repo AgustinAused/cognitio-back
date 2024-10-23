ac36b737216e89331bc2838cbcaa25b99cb2ca4d
Agustin Aused <agus.aused@gmail.com>
2024-10-23 15:04:34 -0300

    Refactor ProgressSchema to include individual completion status for each exercise

--- app/schemas/progress_schm.py
+++ app/schemas/progress_schm.py
@@ -2,35 +2,36 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 
-
 class ProgressBase(BaseModel):
     level: int
 
 
 class ProgressCreated(ProgressBase):
+    ex1_completed: bool
+    ex2_completed: bool
+    ex3_completed: bool
+    ex4_completed: bool
+    ex5_completed: bool
     completed_at: Optional[str] = None
-    ex1_completed = bool
-    ex2_completed = bool
-    ex3_completed = bool
-    ex4_completed = bool
-    ex5_completed = bool
 
 
 class ProgressUpdate(ProgressBase):
+    ex1_completed: Optional[bool] = None
+    ex2_completed: Optional[bool] = None
+    ex3_completed: Optional[bool] = None
+    ex4_completed: Optional[bool] = None
+    ex5_completed: Optional[bool] = None
     completed_at: Optional[str] = None
-    ex1_completed = Optional[bool]
-    ex2_completed = Optional[bool]
-    ex3_completed = Optional[bool]
-    ex4_completed = Optional[bool]
-    ex5_completed = Optional[bool]
 
 
 class ProgressOut(ProgressBase):
     id: int
-    level: int
-    exercises: List[bool]
+    ex1_completed: bool
+    ex2_completed: bool
+    ex3_completed: bool
+    ex4_completed: bool
+    ex5_completed: bool
     completed_at: Optional[str] = None
 
     class Config:
         orm_mode = True
-
