19c0cf3418bc0e17982e1f745a55a5f5006695b0
Agustin Aused <agus.aused@gmail.com>
2024-10-24 22:23:54 -0300

    Refactor ProgressBase schema to include a new field 'type'

--- app/schemas/progress_schm.py
+++ app/schemas/progress_schm.py
@@ -4,33 +4,21 @@ from typing import List, Optional
 
 class ProgressBase(BaseModel):
     level: int
+    type: str
 
 
 class ProgressCreated(ProgressBase):
-    ex1_completed: bool
-    ex2_completed: bool
-    ex3_completed: bool
-    ex4_completed: bool
-    ex5_completed: bool
+    exercises_completed: List[bool]
 
 
 class ProgressUpdate(ProgressBase):
-    ex1_completed: Optional[bool] = None
-    ex2_completed: Optional[bool] = None
-    ex3_completed: Optional[bool] = None
-    ex4_completed: Optional[bool] = None
-    ex5_completed: Optional[bool] = None
+    exercises_completed: Optional[List[bool]] = None
     completed_at: Optional[str] = None
 
 
 class ProgressOut(ProgressBase):
     id: int
-    ex1_completed: bool
-    ex2_completed: bool
-    ex3_completed: bool
-    ex4_completed: bool
-    ex5_completed: bool
     completed_at: Optional[str] = None
-
+    score: float
     class Config:
         from_attributes = True
