5c62e7194e1a5ae4f8c9c035558874204d71079c
Agustin Aused <agus.aused@gmail.com>
2024-10-25 00:19:09 -0300

    Refactor main.py to include drop_db_and_tables function

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 ﻿from fastapi import FastAPI
 from app.routers import user_r, game_r, progress_r 
-from app.database.db import create_db_and_tables
+from app.database.db import create_db_and_tables, drop_db_and_tables
 
 app = FastAPI()
 
 # @app.on_event("startup")
 # async def on_startup():
+#      await drop_db_and_tables()
 #      await create_db_and_tables()
+
+
 
 # Incluir routers (rutas)
 app.include_router(user_r.router,tags=['User'],prefix='/user')
