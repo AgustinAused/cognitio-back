699ee5e2e2e016e29455791df1c0c7493e8e2c8b
Agustin Aused <agus.aused@gmail.com>
2024-10-31 10:40:00 -0300

    Refactor main.py to include image_r router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 ﻿from fastapi import FastAPI
-from app.routers import user_r, game_r, progress_r 
+from app.routers import user_r, game_r, progress_r, image_r
 from fastapi.middleware.cors import CORSMiddleware
 from app.database.db import create_db_and_tables, drop_db_and_tables
 
@@ -25,6 +25,8 @@ app.add_middleware(
 app.include_router(user_r.router,tags=['User'],prefix='/user')
 app.include_router(game_r.router,tags=['Game'],prefix='/game')
 app.include_router(progress_r.router,tags=['Progress'],prefix='/progress')
+app.include_router(image_r.router,tags=['Image'],prefix='/image')
+
 
 @app.get("/")
 def read_root():
