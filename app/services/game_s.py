53f33247acaeaf15e866ae055c3f7601b0b8c14a
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-11-26 17:09:21 -0300

    added missing games

--- app/services/game_s.py
+++ app/services/game_s.py
@@ -1,5 +1,7 @@
 from app.services.strategies.game_strategy import GameStrategy
 from app.services.strategies.partners_contraries import PartnersAndContraries
+from app.services.strategies.read_conclude import ReadAndConclude
+from app.services.strategies.who_was import WhoWas
 import os
 
 API_KEY = os.getenv("OPENAI_API_KEY")
@@ -9,6 +11,12 @@ async def generate_game(number: int,number_excercises: int,difficulty: int):
     match number:
         case 1:
             game : GameStrategy = PartnersAndContraries("app/utils/partners_contaries_prompt.json",API_KEY)
+            return game.generate_game(difficulty,number_excercises)
+        case 2:
+            game : GameStrategy = ReadAndConclude("app/utils/lee_y_concluye.json",API_KEY)
+            return game.generate_game(difficulty,number_excercises)
+        case 3:
+            game : GameStrategy = WhoWas("app/utils/quien_fue.json",API_KEY)
             return game.generate_game(difficulty,number_excercises)
         case _:
             return {"message": "Game not found!"}
---
+++ app/services/strategies/who_was.py
@@ -0,0 +1,37 @@
+from .game_strategy import GameStrategy
+import json
+from openai import OpenAI
+
+
+class WhoWas(GameStrategy):
+    def __init__(self, game_prompt: str ,api_key:str):
+        self.api_key = api_key
+        self.game_prompt = game_prompt
+
+
+    def generate_game(self,difficulty,number_excercises):
+        client:OpenAI = OpenAI()
+        prompt : object = self.build_prompt(difficulty,number_excercises)
+        json_prompt: object = self.get_json()
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content":json.dumps(json_prompt)},
+                {"role": "user", "content": prompt}
+            ],
+            response_format={"type": "json_object"}
+        )
+        response_object = response.choices[0].message.content
+        beautify_response = json.loads(response_object)
+
+        return beautify_response
+    
+    
+
+    def get_json(self):
+        with open(self.game_prompt) as f:
+            return json.load(f)
+        
+    def build_prompt(self,difficulty,number_excercises):
+        return f"Genera {number_excercises} ejercicios de nivel {difficulty} "
+        
